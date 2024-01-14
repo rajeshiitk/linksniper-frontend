b4e4a2a32589caf2fab59a378c1581ebddfb5088
Rajesh Choudhary <rajuvert18@gmail.com>
2024-01-14 22:44:44 +0530

    backend url updated

--- src/components/content/Content.tsx
+++ src/components/content/Content.tsx
@@ -1,7 +1,6 @@
 "use client";
 import Image from "next/image";
 import React, { useState } from "react";
-import Loader from "../loader/Loader";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import { useSelector } from "react-redux";
@@ -17,20 +16,16 @@ function Content() {
       }
   );
 
-  // useEffect(() => {
-  //   if (status === "fulfilled") {
-  //     toast.success("Link Generated");
-  //   }
-  // }, [data, status]);
-
   const [copied, setCopied] = useState(false);
   console.log(data, status);
 
   const handleCopy = ({ shortId }: { shortId: string }): void => {
     if (shortId) {
-      navigator.clipboard.writeText(`http://localhost:3001/${shortId}`);
+      navigator.clipboard.writeText(
+        `${process.env.NEXT_PUBLIC_BACKEND_URL}/${shortId}`
+      );
       setCopied(true);
-      toast.success("Copied to clipboard");
+      toast.success("Copied to clipboard !");
       setTimeout(() => {
         setCopied(false);
       }, 2000);
@@ -58,7 +53,7 @@ function Content() {
                   id="link"
                   className=" z-10 my-auto bg-transparent  text-gray-600 text-sm  md:text-lg font-semibold  w-full h-10   rounded break-all  "
                 >
-                  {`http://localhost:3001/${data?.shortId}`}
+                  {`${process.env.NEXT_PUBLIC_BACKEND_URL}/${data?.shortId}`}
                 </div>
                 <button
                   onClick={() => {
@@ -119,61 +114,6 @@ function Content() {
         </div>
       )}
     </>
-    // <div className="flex w-full justify-center items-center h-screen">
-    //   {/* Card 1: Copy Link */}
-    //   <div className="max-w-sm mx-4 bg-white shadow-lg rounded-lg overflow-hidden">
-    //     <div className="px-6 py-4">
-    //       <div className="font-bold text-xl mb-2">Shortened Link</div>
-    //       <p className="text-gray-700 text-base">{shortUrl}</p>
-    //     </div>
-    //     <div className="px-6 py-4">
-    //       <button
-    //         onClick={handleCopy}
-    //         className={`bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded ${
-    //           copied ? "bg-green-500" : ""
-    //         }`}
-    //       >
-    //         {copied ? "Copied!" : "Copy Link"}
-    //       </button>
-    //     </div>
-    //   </div>
-
-    //   <div className="flex items-stretch mt-2">
-    //     <div className="relative w-full">
-    //       <div className="absolute text-gray-500 flex items-center px-2 border-r h-full"></div>
-    //       <input
-    //         id="link"
-    //         className="pr-24 text-gray-600 bg-gray-100 focus:outline-none focus:border focus:border-indigo-700 font-normal w-full h-10 flex items-center pl-12 text-sm border-gray-300 rounded border"
-    //         value="https://alphad.co/Q4XY3HWXN95"
-    //       />
-    //       <button className="focus:ring-2 focus:ring-offset-2 rounded-md focus:ring-indigo-600 absolute right-0 top-0 transition duration-150 ease-in-out hover:bg-indigo-600 focus:outline-none bg-indigo-700 rounded-r text-white px-5 h-10 text-sm">
-    //         Copy
-    //       </button>
-    //     </div>
-    //   </div>
-
-    //   {/* Card 2: Download QR Code */}
-    //   <div className="max-w-sm mx-4 bg-white shadow-lg rounded-lg overflow-hidden">
-    //     <div className="px-6 py-4">
-    //       <div className="font-bold text-xl mb-2">QR Code</div>
-    //       <div className="text-center">
-    //         <Image src="/generate.svg" alt="qr code" width={150} height={150} />
-    //         {/* <QRCode value={shortUrl} /> */}
-    //       </div>
-    //     </div>
-    //     <div className="px-6 py-4">
-    //       {/* <a
-    //         href={`https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${encodeURIComponent(
-    //           shortUrl
-    //         )}`}
-    //         download="qr-code.png"
-    //         className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded"
-    //       >
-    //         Download QR Code
-    //       </a> */}
-    //     </div>
-    //   </div>
-    // </div>
   );
 }
 
